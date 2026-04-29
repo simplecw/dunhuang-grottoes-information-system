1c81802f512e3985756db9f6ee16f15ca45e75e0
simplecw <simplecw@gmail.com>
2026-04-29 19:46:20 +0800

    Add user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, BigInteger, String, DateTime, SmallInteger
+from sqlalchemy.sql import func
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    username = Column(String(100), nullable=False, unique=True, comment="用户名")
+    password = Column(String(255), nullable=False, comment="密码（明文）")
+    created_at = Column(DateTime, server_default=func.now(), comment="创建时间")
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), comment="更新时间")
+    is_deleted = Column(SmallInteger, default=0, comment="是否删除（0-否 1-是）")
